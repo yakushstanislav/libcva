bbb9ab60f6dcca3029aa20e691fcb3723e68c798
Stanislav Yakush <st.yakush@yandex.ru>
2013-12-28 16:34:47 +0300

    Added assert in applyMatrixFilter3x3()

--- include/Filter.h
+++ include/Filter.h
@@ -48,6 +48,8 @@ T applyMatrixFilter3x3(const Matrix3x3<T>& matrix, const Matrix3x3<T>& convoluti
 template<typename T>
 void applyMatrixFilter3x3(const ImagePlane<T>& src, ImagePlane<T>& dest, const Matrix3x3<T>& convolution, int div = 1, T offset = 0)
 {
+    assert(src.width() == dest.width() && src.height() == src.height());
+
     for (std::size_t x = 1; x < src.width() - 1; x++)
     {
         for (std::size_t y = 1; y < src.height() - 1; y++)
