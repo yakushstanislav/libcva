8b2cac3aa843767bebacc0b00f77cd99ec51af83
Stanislav Yakush <st.yakush@yandex.ru>
2014-01-27 21:45:28 +0300

    Deleted function cropPolygon()

--- include/Cropping.h
+++ include/Cropping.h
@@ -27,66 +27,6 @@ namespace CVA {
 
 namespace Cropping {
 
-// PNPOLY algorithm
-static bool isPointInPolygon(const Point& point, const std::vector<Point>& points)
-{
-    bool result = false;
-
-    for (std::size_t i = 0, j = points.size() - 1; i < points.size(); i++)
-    {
-        const std::size_t x = point.getX();
-        const std::size_t y = point.getY();
-        const std::size_t currX = points[i].getX();
-        const std::size_t currY = points[i].getY();
-        const std::size_t prevX = points[j].getX();
-        const std::size_t prevY = points[j].getY();
-
-        if (currY < y && prevY >= y || prevY < y && currY >= y)
-        {
-            if (currX + (y - currY) / (prevY - currY) * (prevX - currX) < x)
-            {
-                result = !result;
-            }
-        }
-
-        j = i;
-    }
-
-    return result;
-}
-
-template<typename T>
-bool cropPolygon(const ImagePlane<T>& src, ImagePlane<T>& dest, const std::vector<Point>& points)
-{
-    assert(points.size() >= 3);
-
-    const auto minMaxPointValueX = std::minmax_element(points.begin(), points.end(),
-        [](Point pt1, Point pt2) { return pt1.getX() < pt2.getX(); });
-
-    const auto minMaxPointValueY = std::minmax_element(points.begin(), points.end(),
-        [](Point pt1, Point pt2) { return pt1.getY() < pt2.getY(); });
-
-    const std::size_t minPointValueX = minMaxPointValueX.first->getX();
-    const std::size_t maxPointValueX = minMaxPointValueX.second->getX();
-
-    const std::size_t minPointValueY = minMaxPointValueY.first->getY();
-    const std::size_t maxPointValueY = minMaxPointValueY.second->getY();
-
-    assert(dest.width() >= (maxPointValueX - minPointValueX) &&
-        dest.height() >= (maxPointValueY - minPointValueY));
-
-    for (std::size_t x = 0; x < src.width(); x++)
-    {
-        for (std::size_t y = 0; y < src.height(); y++)
-        {
-            if (isPointInPolygon(Point(x, y), points))
-            {
-                dest.setPixel(x - minPointValueX - 1, y - minPointValueY - 1, src.getPixel(x, y));
-            }
-        }
-    }
-}
-
 template<typename T>
 void cropRectangle(const ImagePlane<T>& src, ImagePlane<T>& dest, const Point& start, const Point& end)
 {
